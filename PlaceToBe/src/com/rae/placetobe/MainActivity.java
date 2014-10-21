6d8399be4e1c80155f365d3fe1d2689de5ea7524
RobertBakic <robert.bakic@gmail.com>
2014-10-21 15:26:10 +0200

    Prise de la photo

--- PlaceToBe/src/com/rae/placetobe/MainActivity.java
+++ PlaceToBe/src/com/rae/placetobe/MainActivity.java
@@ -1,17 +1,26 @@
 package com.rae.placetobe;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity
 {
+	private ImageView mImageView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mImageView = (ImageView) findViewById(R.id.imageViewPhoto);
 	}
 
 	@Override
@@ -20,6 +29,33 @@ public class MainActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void showPhoto(View view)
+	{
+		dispatchTakePictureIntent();
+	}
+
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+
+	private void dispatchTakePictureIntent()
+	{
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+		{
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+		{
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap) extras.get("data");
+			mImageView.setImageBitmap(imageBitmap);
+		}
 	}
 
 	@Override
@@ -35,4 +71,5 @@ public class MainActivity extends Activity
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
