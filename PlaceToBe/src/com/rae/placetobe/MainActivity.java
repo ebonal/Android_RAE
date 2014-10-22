f19e231ee930a6fbabf03a868b838ba82a867574
AnthonyFontaine <anthonyfontaine34@gmail.com>
2014-10-22 15:08:03 +0200

    debut menu tiroir

--- PlaceToBe/src/com/rae/placetobe/MainActivity.java
+++ PlaceToBe/src/com/rae/placetobe/MainActivity.java
@@ -2,25 +2,76 @@ package com.rae.placetobe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends Activity
 {
 	private ImageView mImageView;
+	
+	/** Variable pour le Menu tiroir */
+	// Vue pour mon menu tiroir
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	// Variable pour afficher mon menu
+	private String[] mMainNavTitles;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.drawer_layout);
 
 		mImageView = (ImageView) findViewById(R.id.imageViewPhoto);
+		
+		// Initialisation des valeures pour le menu tiroir
+		mMainNavTitles = getResources().getStringArray(R.array.main_nav_array);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        
+        // Définition de mon adapter pour ma list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+        		R.layout.drawer_list_item, mMainNavTitles));
+        // Click listener sur ma list list view
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+                ){
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+            	invalidateOptionsMenu();
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+            	invalidateOptionsMenu();
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
 
 	@Override
@@ -58,6 +109,7 @@ public class MainActivity extends Activity
 		}
 	}
 
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
@@ -70,6 +122,46 @@ public class MainActivity extends Activity
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+    
+    
+	
+	
+	/** Menu tiroir */
+	
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+	
+	// Swaps fragments in the main content view
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView parent, View view, int position, long id) {
+	        selectItem(position);
+	    }
+	}
+	
+	// Swaps fragments in the main content view
+	private void selectItem(int position) {
+	    // TODO
 	}
 
 }
